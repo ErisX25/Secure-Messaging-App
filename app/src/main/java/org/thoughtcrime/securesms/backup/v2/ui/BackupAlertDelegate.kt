e4ed14b114009925375b97f57bc1e547eaa68640
Pheosi <158743773+Pheosi@users.noreply.github.com>
2024-11-25 14:49:29 +0530

    First commit

---
+++ app/src/main/java/org/thoughtcrime/securesms/backup/v2/ui/BackupAlertDelegate.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2024 Signal Messenger, LLC
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+package org.thoughtcrime.securesms.backup.v2.ui
+
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.coroutineScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.launch
+import org.thoughtcrime.securesms.backup.v2.BackupRepository
+import org.thoughtcrime.securesms.keyvalue.SignalStore
+
+/**
+ * Delegate that controls whether and which backup alert sheet is displayed.
+ */
+object BackupAlertDelegate {
+  @JvmStatic
+  fun delegate(fragmentManager: FragmentManager, lifecycle: Lifecycle) {
+    lifecycle.coroutineScope.launch {
+      lifecycle.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+        if (BackupRepository.shouldDisplayBackupFailedSheet()) {
+          BackupAlertBottomSheet.create(BackupAlert.CouldNotCompleteBackup(daysSinceLastBackup = SignalStore.backup.daysSinceLastBackup)).show(fragmentManager, null)
+        }
+
+        // TODO [backups] Check if media will be deleted within 24hrs and display warning sheet.
+
+        // TODO [backups]
+        // Get unnotified backup download failures & display sheet
+      }
+    }
+  }
+}
---
+++ libsignal-service/src/main/java/org/whispersystems/signalservice/internal/websocket/LibSignalNetworkExtensions.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2024 Signal Messenger, LLC
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+@file:JvmName("LibSignalNetworkExtensions")
+
+package org.whispersystems.signalservice.internal.websocket
+
+import org.signal.core.util.orNull
+import org.signal.libsignal.net.ChatService
+import org.signal.libsignal.net.Network
+import org.whispersystems.signalservice.api.util.CredentialsProvider
+import org.whispersystems.signalservice.internal.configuration.SignalServiceConfiguration
+
+/**
+ * Helper method to create a ChatService with optional credentials.
+ */
+fun Network.createChatService(
+  credentialsProvider: CredentialsProvider? = null,
+  receiveStories: Boolean
+): ChatService {
+  val username = credentialsProvider?.username ?: ""
+  val password = credentialsProvider?.password ?: ""
+  return if (username.isEmpty() && password.isEmpty()) {
+    this.createUnauthChatService(null)
+  } else {
+    this.createAuthChatService(username, password, receiveStories, null)
+  }
+}
+
+/**
+ * Helper method to apply settings from the SignalServiceConfiguration.
+ */
+fun Network.applyConfiguration(config: SignalServiceConfiguration) {
+  val proxy = config.signalProxy.orNull()
+
+  if (proxy == null) {
+    this.clearProxy()
+  } else {
+    this.setProxy(proxy.host, proxy.port)
+  }
+
+  this.setCensorshipCircumventionEnabled(config.censored)
+}
